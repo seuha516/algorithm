65b56d0ece84b5aa2f908e9380c594d1da07c075
seuha516 <seuha516@naver.com>
2021-02-18 01:34:06 +0900

    bronze

---
+++ Baekjoon OJ/B5 ~ B1/20215 Cutting Corners.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <stack>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main(){
+	
+	double a,b; scanf("%lf %lf",&a,&b); printf("%.10lf",a+b-sqrt(a*a+b*b));
+	
+	return 0;
+}
+
---
+++ Baekjoon OJ/B5 ~ B1/20353 Artium.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <stack>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+int main(){
+	
+	double x; scanf("%lf",&x); printf("%.15lf",4*sqrt(x));
+
+	return 0;
+}
+

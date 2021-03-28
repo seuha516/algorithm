2be4ad8dba9ed36496e2c26e9fa6e3c2414352a3
seuha516 <seuha516@naver.com>
2021-03-28 23:36:57 +0900

    #711

---
+++ Codeforces/Codeforces Round #711 (Div. 2)/E.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <set>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <set>
+#include <cstdlib>
+#include <map>
+#include <unordered_map>
+#define ll long long
+#define ull unsigned long long
+
+using namespace std;
+
+
+int main(){
+	
+	int TC; scanf("%d",&TC);
+	while(TC--){
+		
+		vector<int> v;
+		
+		int n; scanf("%d",&n);
+		int n,k; scanf("%d %d",&n,&k);
+		
+		for(int i=0;i<n;i++){
+			int x; scanf("%d",&x); v.push_back(x); 
+		}
+				
+		
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
